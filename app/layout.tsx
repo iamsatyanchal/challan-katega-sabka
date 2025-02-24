f14baddf1d86ee2f85796c0ae11ca7182dca32c1
Satyanchal <62104921+iamsatyanchal@users.noreply.github.com>
2025-02-24 17:34:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'challan kategaa sabka',
+  description: 'sabki kategi very soon',
+  generator: 'satyanchal',
 }
 
 export default function RootLayout({
